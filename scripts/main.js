064400633273e04821642e5d5d7c4664b393a9b7
technolygames <69221500+technolygames@users.noreply.github.com>
2022-05-02 17:10:31 -0500

    Quinto commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,11 @@
 'use strict';
 
-/*function cal(){
-    var dato1=document.getElementById("texto1").value;
-    var dato2=document.getElementById("texto2").value;
-
-    var parsedato1=parseInt(dato1);
-    var parsedato2=parseInt(dato2);
-
-    var resultado=parsedato1*parsedato2;
-
-    document.getElementById("perrafo").innerHTML=resultado;
-}
-
-var ola=document.getElementById("div1");
-var boton=document.querySelector('#click');
-
-boton.addEventListener("click",()=>{
-    var ver=new hola();
-    ver.oladiv();
-});
-
-function hola(){
-    this.oladiv=()=>{
-        ola.innerHTML="ola";
-    }
-}*/
-
 var text=document.getElementById("parrafo");
 function texto(){
     text.innerHTML="uwun't"
 }
+
+let input_element=document.querySelector("input");
+input_element.addEventListener("keyup",()=>{
+    input_element.setAttribute("value",input_element.value);
+})
