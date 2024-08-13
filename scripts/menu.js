9f4669fae41447b4730f4f2d78d1f0182f550386
arrenualdo123 <jesearreola13@gmail.com>
2024-08-12 23:47:48 -0600

    contactos

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerIcon = document.getElementById('hamburger-icon');
-    const dropdownMenu = document.getElementById('hamburger-dropdown');
+document.addEventListener("DOMContentLoaded", function() {
+    var menuHamburguesa = document.getElementById("menuHamburguesa");
+    var hamburgerIcon = document.getElementById("hamburgerIcon");
+    var hamburgerDropdown = document.getElementById("hamburgerDropdown");
 
-    hamburgerIcon.addEventListener('click', function () {
-        dropdownMenu.classList.toggle('show');
+    hamburgerIcon.addEventListener("click", function() {
+        if (hamburgerDropdown.style.display === "block") {
+            hamburgerDropdown.style.display = "none";
+        } else {
+            hamburgerDropdown.style.display = "block";
+        }
     });
 
     // Cierra el menú si se hace clic fuera de él
-    window.addEventListener('click', function (event) {
-        if (!hamburgerIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.classList.remove('show');
+    window.addEventListener("click", function(event) {
+        if (!menuHamburguesa.contains(event.target)) {
+            hamburgerDropdown.style.display = "none";
         }
     });
 });
